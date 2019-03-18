adaa83f8682191ef5ecd720c054a3d1df6de9a0e
Morgan Dorfman <mdorfman21@gmail.com>
2019-03-18 14:24:16 -0400

    added comments on code

--- liri.js
+++ liri.js
@@ -1,3 +1,4 @@
+//adding the require statements to get libraries
 require("dotenv").config();
 var keys = require("./keys.js");
 var Spotify = require("node-spotify-api");
@@ -6,12 +7,18 @@ var moment = require("moment");
 var spotify = new Spotify(keys.spotify);
 moment().format();
 
+//if they user puts nothing then it gives back an error
 if (process.argv.length == 2) {
   return console.log("bad args");
 }
 
+//set the operation term to the same element in the array everytime
 var operation = process.argv[2];
+
+//initialize the search terms so that I can use this to call the API's
 var searchTerms = "";
+
+//for each element after the first 3, make it into a string and add a + sign unless it's the final element in the array
 process.argv.forEach((x, index) => {
   if (index > 2 && index == process.argv.length - 1) {
     searchTerms += x;
@@ -22,6 +29,8 @@ process.argv.forEach((x, index) => {
 
 console.log(searchTerms);
 console.log(operation);
+
+//create a switch to have the different operation cases
 switch (operation) {
   case "concert-this": {
     break;

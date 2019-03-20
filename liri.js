7b42ce9f8df5be75792ac4b2c481253eaf198404
Morgan Dorfman <mdorfman21@gmail.com>
2019-03-20 14:31:22 -0400

    finsihed the events and started the spotify search

--- liri.js
+++ liri.js
@@ -33,9 +33,70 @@ console.log(operation);
 //create a switch to have the different operation cases
 switch (operation) {
   case "concert-this": {
+    //if there is no artist entered return an error
+    if (searchTerms === "") {
+      return console.log("You need to add an artist!");
+    }
+    var queryURL =
+      "https://rest.bandsintown.com/artists/" +
+      searchTerms +
+      "/events?app_id=codingbootcamp";
+
+    axios
+      .get(queryURL)
+      .then(response => {
+        console.log(response.data);
+        var objectResponse = response.data;
+        for (var key in objectResponse) {
+          if (objectResponse.hasOwnProperty) {
+            console.log(
+              "========================================================"
+            );
+            console.log(
+              "The name of the venue is",
+              objectResponse[key].venue.name
+            );
+            console.log(
+              "It's located at",
+              objectResponse[key].venue.city,
+              objectResponse[key].venue.region,
+              ",",
+              objectResponse[key].venue.country
+            );
+            console.log(
+              "On",
+              moment(objectResponse[key].datetime).format("MMM Do YYYY, h:mm a")
+            );
+          }
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      });
     break;
   }
   case "spotify-this-song": {
+    if (searchTerms === "") {
+      return console.log("Please enter a song name");
+    }
+    var songName = searchTerms.split("+").join(" ");
+    console.log(songName);
+    spotify
+      .search({
+        type: "track",
+        query: songName
+      })
+      .then(response => {
+        var objectResponse = response.tracks.items;
+        console.log(response.tracks.items);
+        if (objectResponse.length === 0) {
+          return console.log("'The Sign' by Ace of Base");
+        }
+        console.log("The artist is", objectResponse[0].album.artists[0].name);
+      })
+      .catch(err => {
+        console.log(err);
+      });
     break;
   }
   case "movie-this": {
